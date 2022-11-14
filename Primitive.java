7c26e86157d89767569b8728118d61370c968b1d
urana03 <uhackninja@gmail.com>
2022-11-14 13:18:23 +0530

    Add:Program to display the default values of all primitive data types

---
+++ Primitive.java
@@ -0,0 +1,22 @@
+package com.bridgelabz.dayone;
+ 
+public class Primitive {
+	 static boolean valOne;
+	 static double numTwo;
+	 static float  numThree;
+	 static int numFour;
+	 static long numFive;
+	 static String stringOne;
+	 static char valChar;
+	 
+	 public static void main(String args[]) {
+	        System.out.println("Default Values:");
+	        System.out.println("Boolean = "+ valOne);
+	        System.out.println("Double = "+ numTwo);
+	        System.out.println("Float = "+ numThree);
+	        System.out.println("Integer = "+ numFour);
+	        System.out.println("Long = "+ numFive);
+	        System.out.println("String = "+ stringOne);
+	        System.out.println("Character = "+ valChar);
+	    }
+}
