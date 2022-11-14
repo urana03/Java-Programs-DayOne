2a13244e011d1da05850c0e2203a3ae867f11c9e
urana03 <uhackninja@gmail.com>
2022-11-14 13:28:50 +0530

    Add:Program to get the name using command line

---
+++ Name.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.dayone;
+import java.util.*;
+
+public class Name {
+
+	 public static void main(String args[]) {
+	        Scanner scan = new Scanner(System.in);
+	        System.out.print("Enter your name:");
+	        String name = scan.nextLine();
+	        System.out.println("Your name is "+ name+".");
+	    
+	    }
+}
